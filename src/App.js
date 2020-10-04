1d8a752a61518e4fe98c702a2e1f54d7dbdd8eba
unknown <daniel.nunez.pessoal@gmail.com>
2020-10-04 18:36:23 -0300

    Setup: Auth & Unauth components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useEffect, lazy, Suspense } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+
 import { useUser } from "./context/user-context";
+
+const loadAuthenticatedApp = () => import("./AuthenticatedApp");
+const AuthenticatedApp = lazy(loadAuthenticatedApp);
+const UnauthenticatedApp = lazy(() => import("./UnauthenticatedApp"));
 
 const App = () => {
   const { userInfo } = useUser();
 
+  useEffect(() => {
+    loadAuthenticatedApp();
+  }, []);
+
   console.log(userInfo);
 
-  return <div></div>;
+  return (
+    <main>
+      <Suspense fallback={<div>loading</div>}>
+        <Router>
+          {userInfo ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+        </Router>
+      </Suspense>
+    </main>
+  );
 };
 
 export default App;
---
+++ src/AuthenticatedApp.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
+
+const AuthenticatedApp = () => <Switch></Switch>;
+
+export default AuthenticatedApp;
